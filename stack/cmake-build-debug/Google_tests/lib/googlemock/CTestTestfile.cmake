746158436c340003108df1c1e8f695ec32309c7b
JingfengS <jingfeng050106@gmail.com>
2024-05-12 13:03:14 +0800

    exclude cmake-debug files

--- stack/cmake-build-debug/Google_tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jingfengsun/Documents/data structures/stack/Google_tests/lib/googlemock
-# Build directory: /Users/jingfengsun/Documents/data structures/stack/cmake-build-debug/Google_tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
